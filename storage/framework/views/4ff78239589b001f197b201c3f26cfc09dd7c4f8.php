2237c976b9536fb017292d562b9c31d32b188792
Abdul Samad <abdulsamad15cs56@gmail.com>
2023-02-15 19:52:02 +0500

    Initial commit

---
+++ storage/framework/views/4ff78239589b001f197b201c3f26cfc09dd7c4f8.php
@@ -0,0 +1,5 @@
+<h1>Listing Single</h1>
+
+<h2><?php echo e($listing['title']); ?></h2>
+<p><?php echo e($listing['description']); ?></p>
+<a href="/">Home Page</a><?php /**PATH C:\xampp\htdocs\Laravel-crash-course\laragigs\resources\views/listing.blade.php ENDPATH**/ ?>
